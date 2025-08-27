348e1420c74c58f25131622968a91b32df802e26
Himani-rwt <himanirawat318@gmail.com>
2025-08-27 15:00:49 +0000

    container config done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,7 @@
+const accountId = 144533   //const value does not change later
+let accountEmail ="himani@google.com"
+var accountPassword ="123"   //don't use var because of function scope 
+accountCity = "jaipur"  //this also a variable but use let of variable is good learning
+let accountState;
+
+console.table([[accountId,accountEmail,accountPassword,accountCity,accountState]]);
