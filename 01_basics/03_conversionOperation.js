0e3d8b53ea837d42996a2b7bb4a19f4f8af7b146
Himani-rwt <himanirawat318@gmail.com>
2025-08-28 11:12:06 +0000

    container config done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let number="33ab"
+console.log(typeof(number));
+let contonum = Number(number);
+console.log(typeof(contonum));
+console.log(contonum)
+//NaN means not a number
+// if true =>1 ow 0
+letloggedIn = ""
+let booleanIsLoggedIn  = Boolean(letloggedIn)
+console.log(booleanIsLoggedIn);
+
+//1 =>true ; 0=>false
+//"" =>false; "himani"=>True;
+
+let anynumber = 3;
+let contostring = String(anynumber)
+console.log(contostring);
+console.log(typeof contostring);
+
+// **************Operations***************
+
+let value=5;
+let negvalue = -value;
+console.log(negvalue);
+
+console.log('3'+2+2);  //ans 322 act first comes in
+console.log(3+2+"2");  //ans 52
+
+
+
+  
